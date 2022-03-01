0f2d91ff815eac6a644307f28e46e91bedb6af72
Michael Campos <michael_c55@berkeley.edu>
2022-02-28 21:43:07 -0800

    Removed asseretsgsdgshg

--- app/src/test/java/com/example/bottomnav/ParseTest.java
+++ app/src/test/java/com/example/bottomnav/ParseTest.java
@@ -1,12 +1,9 @@
-package com.company;
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+package com.example.bottomnav;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
 
 class ParseTest {
 
@@ -17,17 +14,17 @@ class ParseTest {
                 "CAN_ID3 = 0x64;";
         Parse test = new Parse(code);
 
-        Assertions.assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID"));
-        Assertions.assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID2"));
-        Assertions.assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID3"));
+        assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID"));
+        assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID2"));
+        assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID3"));
 
         IllegalAccessError exception1 = assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID"));
         IllegalAccessError exception2 = assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID"));
         IllegalAccessError exception3 = assertThrows(IllegalAccessError.class, () -> test.get("CAN_ID"));
 
-        Assert.assertEquals("The name does not exist!", exception1.getMessage());
-        Assert.assertEquals("The name does not exist!", exception2.getMessage());
-        Assert.assertEquals("The name does not exist!", exception3.getMessage());
+        assertEquals("The name does not exist!", exception1.getMessage());
+        assertEquals("The name does not exist!", exception2.getMessage());
+        assertEquals("The name does not exist!", exception3.getMessage());
     }
 
     @Test
@@ -40,21 +37,21 @@ class ParseTest {
 
         HashMap<String, Contents> repository = test.repo;
 
-        Assert.assertEquals(true, repository.containsKey("CAN_ID"));
-        Assert.assertEquals(true, repository.containsKey("CAN_ID2"));
-        Assert.assertEquals(true, repository.containsKey("CAN_ID3"));
+        assertEquals(true, repository.containsKey("CAN_ID"));
+        assertEquals(true, repository.containsKey("CAN_ID2"));
+        assertEquals(true, repository.containsKey("CAN_ID3"));
 
-        Assert.assertEquals("const", test.get("CAN_ID").typeQualifer);
-        Assert.assertEquals("const", test.get("CAN_ID2").typeQualifer);
-        Assert.assertEquals("const", test.get("CAN_ID3").typeQualifer);
+        assertEquals("const", test.get("CAN_ID").typeQualifer);
+        assertEquals("const", test.get("CAN_ID2").typeQualifer);
+        assertEquals("const", test.get("CAN_ID3").typeQualifer);
 
-        Assert.assertEquals("uint16_t", test.get("CAN_ID").type);
-        Assert.assertEquals("uint16_t", test.get("CAN_ID2").type);
-        Assert.assertEquals("uint16_t", test.get("CAN_ID3").type);
+        assertEquals("uint16_t", test.get("CAN_ID").type);
+        assertEquals("uint16_t", test.get("CAN_ID2").type);
+        assertEquals("uint16_t", test.get("CAN_ID3").type);
 
-        Assert.assertEquals("0x16", test.get("CAN_ID").value);
-        Assert.assertEquals("0x32", test.get("CAN_ID2").value);
-        Assert.assertEquals("0x64", test.get("CAN_ID3").value);
+        assertEquals("0x16", test.get("CAN_ID").value);
+        assertEquals("0x32", test.get("CAN_ID2").value);
+        assertEquals("0x64", test.get("CAN_ID3").value);
     }
 
     @Test
@@ -67,21 +64,21 @@ class ParseTest {
 
         HashMap<String, Contents> repository = test.repo;
 
-        Assert.assertEquals(true, repository.containsKey("CAN_ID1"));
-        Assert.assertEquals(true, repository.containsKey("CAP_ID2"));
-        Assert.assertEquals(true, repository.containsKey("CAN_ID3"));
+        assertEquals(true, repository.containsKey("CAN_ID1"));
+        assertEquals(true, repository.containsKey("CAP_ID2"));
+        assertEquals(true, repository.containsKey("CAN_ID3"));
 
-        Assert.assertEquals("const", test.get("CAN_ID1").typeQualifer);
-        Assert.assertEquals("const", test.get("CAP_ID2").typeQualifer);
-        Assert.assertEquals("const", test.get("CAN_ID3").typeQualifer);
+        assertEquals("const", test.get("CAN_ID1").typeQualifer);
+        assertEquals("const", test.get("CAP_ID2").typeQualifer);
+        assertEquals("const", test.get("CAN_ID3").typeQualifer);
 
-        Assert.assertEquals("uint16_t", test.get("CAN_ID1").type);
-        Assert.assertEquals("uint32_t", test.get("CAP_ID2").type);
-        Assert.assertEquals("uint32_t", test.get("CAN_ID3").type);
+        assertEquals("uint16_t", test.get("CAN_ID1").type);
+        assertEquals("uint32_t", test.get("CAP_ID2").type);
+        assertEquals("uint32_t", test.get("CAN_ID3").type);
 
-        Assert.assertEquals("0x16", test.get("CAN_ID1").value);
-        Assert.assertEquals("0x18", test.get("CAP_ID2").value);
-        Assert.assertEquals("0x64", test.get("CAN_ID3").value);
+        assertEquals("0x16", test.get("CAN_ID1").value);
+        assertEquals("0x18", test.get("CAP_ID2").value);
+        assertEquals("0x64", test.get("CAN_ID3").value);
     }
 
     @Test
@@ -91,16 +88,16 @@ class ParseTest {
                 "const uint16_t CAN_ID3 = 0x64;";
         Parse test = new Parse(code);
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> test.getByType("cons"));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> test.getByType("co"));
-        Assertions.assertThrows(IllegalArgumentException.class, () -> test.getByType("cns"));
+        assertThrows(IllegalArgumentException.class, () -> test.getByType("cons"));
+        assertThrows(IllegalArgumentException.class, () -> test.getByType("co"));
+        assertThrows(IllegalArgumentException.class, () -> test.getByType("cns"));
 
         ArrayList<Contents> list = test.getByType("uint16_t");
-        Assert.assertEquals("0x16", list.get(0).value);
-        Assert.assertEquals("0x64", list.get(1).value);
+        assertEquals("0x16", list.get(0).value);
+        assertEquals("0x64", list.get(1).value);
 
         ArrayList<Contents> list2 = test.getByType("uint32_t");
-        Assert.assertEquals("0x32", list2.get(0).value);
+        assertEquals("0x32", list2.get(0).value);
     }
 
 }
