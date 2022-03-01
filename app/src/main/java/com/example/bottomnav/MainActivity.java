074f5dfb3533498b1021035016b6c79cae321f29
Joseph <90059091+JL-Money@users.noreply.github.com>
2022-02-28 19:07:02 -0800

    Update app/src/main/java/com/example/bottomnav/MainActivity.java
    Agreed
    
    Co-authored-by: Richard Lin <elpato25@gmail.com>

--- app/src/main/java/com/example/bottomnav/MainActivity.java
+++ app/src/main/java/com/example/bottomnav/MainActivity.java
@@ -17,8 +17,8 @@ private ActivityMainBinding binding;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-     binding = ActivityMainBinding.inflate(getLayoutInflater());
-     setContentView(binding.getRoot());
+         binding = ActivityMainBinding.inflate(getLayoutInflater());
+         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
