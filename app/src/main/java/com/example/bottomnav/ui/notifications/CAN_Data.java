b665021f328d1ed11b52471e47ee266ad5530b08
Brian Zheng <zbrian2040@berkeley.edu>
2022-06-15 21:42:06 -0700

    Finished CAN_Data

--- app/src/main/java/com/example/bottomnav/ui/notifications/CAN_Data.java
+++ app/src/main/java/com/example/bottomnav/ui/notifications/CAN_Data.java
@@ -1,65 +1,49 @@
 package com.example.bottomnav.ui.notifications;
 
-class CAN_Data {
 
-    Integer id;
-    Integer data;
-    Integer len;
-    public CAN_Data(Integer id,Integer data, Integer len ){
+class CAN_Data {
+    int id;
+    int[] data;
+
+    public CAN_Data(int id, int[] data) {
         this.id = id;
         this.data = data;
-        this.len = len;
     }
-    public static CAN_Data decode(String raw){
-        Integer id = -1;
-        Integer len;
-        Integer data;
+
+    public static CAN_Data decode(String raw) {
+        int id = -1;
+        int len;
+
         int index = -1;
-        if(raw.substring(0,1).equals('t') || raw.substring(0,1).equals('r')){
+        if (raw.charAt(0) == 't' || raw.charAt(0) == 'r') {
             id = Integer.parseInt(raw.substring(1, 4), 16);
             index = 4;
         }
-        else if(raw.substring(0,1).equals('T') || raw.substring(0,1).equals('R')){
+        else if (raw.charAt(0) == 'T' || raw.charAt(0) == 'R') {
             id = Integer.parseInt(raw.substring(1, 9), 16);
             index = 9;
         }
-        else{
+        else {
             return null;
         }
 
-        if((index != -1 ) && (id != -1)) {
-            len = Integer.parseInt(raw.substring(index, index + 1), 16);
-            data = Integer.parseInt(raw.substring(index + 1, raw.length()), 16);
-            CAN_Data decoded = new CAN_Data(id, data, len);
-            return decoded;
-        }
-        else{
-            return null;
-        }
+        len = Integer.parseInt(raw.substring(index, index + 1), 16);
+        int[] data = new int[len];
 
+        for(int x = 0;x<len;x+=1) {
+            data[x] = Integer.parseInt(raw.substring(2*x +index+1, 2*x+2+index+1), 16);
+        }
+        return new CAN_Data(id, data);
     }
-    public Integer getId(){
+
+    public int getId(){
         return id;
     }
 
-    public Integer getData() {
+    public int[] getData() {
         return data;
     }
 
-    public Integer getLen() {
-        return len;
-    }
 
-    public void setData(Integer data) {
-        this.data = data;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setLen(Integer len) {
-        this.len = len;
-    }
 
 }
